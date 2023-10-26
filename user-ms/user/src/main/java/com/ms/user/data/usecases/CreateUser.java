b0a0366bb686b5fe54ea58cd32ed6b636f4ee005
luan <luanbianchine1@gmail.com>
2023-10-26 14:08:53 -0300

    feat: register users in db

--- user-ms/user/src/main/java/com/ms/user/data/usecases/CreateUser.java
+++ user-ms/user/src/main/java/com/ms/user/data/usecases/CreateUser.java
@@ -3,6 +3,7 @@ package com.ms.user.data.usecases;
 import com.ms.user.core.dtos.UserDto;
 import com.ms.user.infra.repositories.UserRepository;
 import com.ms.user.models.User;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +11,12 @@ import org.springframework.stereotype.Service;
 public class CreateUser {
     @Autowired
     private UserRepository repository;
-
+    @Transactional
     public User perform(UserDto data) {
         User newUser = new User();
         newUser.setName(data.name());
         newUser.setEmail(data.email());
+        repository.save(newUser);
         return newUser;
     }
 }
